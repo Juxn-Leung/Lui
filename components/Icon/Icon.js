d8ef609d5fc2dc02e1db6d6462647ca5b7776a52
xuan <1466098865@qq.com>
2021-02-22 22:04:58 +0800

    更新

---
+++ components/Icon/Icon.js
@@ -0,0 +1,37 @@
+// components/Icon.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    label:{
+      type:String
+    },
+
+    type:{
+      type:String
+    },
+
+    color:{
+      type:String
+    },
+    
+    size:{
+      type:String
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/Icon/Icon.js
+++ pages/Icon/Icon.js
@@ -1,4 +1,4 @@
-// pages/index/index.js
+// pages/Icon/Icon.js
 Page({
 
   /**
